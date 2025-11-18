b66e1aa05206afe450407685cbfc3e8a6d1006a7
KGyeong <rlarudals61@naver.com>
2025-11-18 17:31:44 +0900

    feat: tanstack query devtools 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react';
 
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 
 import { ToastProvider } from './components/ui/composites/Toast';
 import ErrorBoundary from './feature/error/components/ErrorBoundary';
+import { queryClient } from './lib/queryClient';
 import { ThemeProvider } from './providers/ThemeProvider';
 import { router } from './router';
 import './lib/env';
@@ -13,12 +16,15 @@ import './styles/tokens.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider>
-      <ErrorBoundary>
-        <ToastProvider>
-          <RouterProvider router={router} />
-        </ToastProvider>
-      </ErrorBoundary>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <ErrorBoundary>
+          <ToastProvider>
+            <RouterProvider router={router} />
+          </ToastProvider>
+        </ErrorBoundary>
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>
 );
