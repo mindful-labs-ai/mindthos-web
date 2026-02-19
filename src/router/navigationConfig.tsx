013a2050767bda8b77bc673cf3dacf58c63ef85e
mindfullabsGyeongminkim1 <gyeongmin.kim@mindfullabs.ai>
2026-02-19 11:56:45 +0900

    fix: pnpm pre-push

--- src/router/navigationConfig.tsx
+++ src/router/navigationConfig.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 import {
-  SideAnalysisIcon,
-  SideCalendarIcon,
+  // SideAnalysisIcon,
+  // SideCalendarIcon,
   SideClientIcon,
   SideGenogramIcon,
   SideHelpIcon,
